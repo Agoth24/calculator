0dbd55ce4afbff9d3bc33f8af19c62288936fd8e
Agoth24 <agoth.a3@gmail.com>
2025-06-20 22:43:21 -0600

    Some more work and refactoring buggy code
    We're getting there...

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
+// OPERATIONS
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => {
   if (b !== 0) {
     return a / b;
-  } return 'Error'
+  }
+  return "Error";
 };
+const operate = (a, b, operation) => operation(a, b);
+// END OF OPERATIONS
+
+// SELECTORS
+let outputScreen = document.querySelector(".current-output");
+const buttonContainer = document.querySelector(".button-container");
+let currentInput = '';
+
+const populateScreen = (element) => {
+  outputScreen.textContent = element;
+};
+
+
+// EVENT DELEGATION
+buttonContainer.addEventListener("click", (e) => {
+  if (!e.target.matches("button")) return;
+
+  const value = e.target.dataset.value;
+
+  if (value === "AC") {
+    currentOutput = "";
+    outputScreen.textContent = ""
+  }
+});
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.dataset)
+    currentInput += button.textContent;
+    outputScreen.textContent = currentInput;
+  });
+});
